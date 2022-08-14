57e043a397cb64841e22ec768a80348f86a33c4d
ramadan82 <ramadan.isa@gmail.com>
2022-08-14 19:46:21 +0100

    modified mongo uri

--- server/dbCon.js
+++ server/dbCon.js
@@ -4,8 +4,6 @@ const { options: connectionParam } = {
   useNewUrlParser: true,
 };
 
-const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.a8zi6ab.mongodb.net/auth-tuts?retryWrites=true&w=majority`;
-
-const dbCon = mongoose.connect(url, connectionParam);
+const dbCon = mongoose.connect(process.env.DB_URI, connectionParam);
 
 module.exports = dbCon;
