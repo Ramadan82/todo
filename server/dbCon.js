92d8067f75f917e2a827e79096f68138b3b8899e
ramadan82 <ramadan.isa@gmail.com>
2022-08-14 19:30:43 +0100

    modified .env

--- server/dbCon.js
+++ server/dbCon.js
@@ -4,6 +4,8 @@ const { options: connectionParam } = {
   useNewUrlParser: true,
 };
 
-const dbCon = mongoose.connect(process.env.URI, connectionParam);
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.a8zi6ab.mongodb.net/auth-tuts?retryWrites=true&w=majority`;
+
+const dbCon = mongoose.connect(url, connectionParam);
 
 module.exports = dbCon;
